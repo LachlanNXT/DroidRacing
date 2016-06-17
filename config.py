199cd55f2fcd8cf1a559857b9372333451c291ed
Lindsay <lindsayawatt@gmail.com>
2016-06-17 19:26:31 +1000

    baud rate updated

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ DEBUG = True
 
 # used by DroidControl
 SERIAL_NAME = '/dev/ttyUSB0'
-SERIAL_BAUD_RATE = 9600 # TODO: reflash arduino - increase baud to 115200
 SERIAL_TIMEOUT = 0.1 # seconds
 QUEUE_SLEEP_TIME = 0.025
