d2824656c07f6b9d7017c7484baaeced9d5e7d41
Lindsay <lindsayawatt@gmail.com>
2016-06-17 17:39:00 +1000

    added clean up

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ droid = DroidControl.DroidControlThread(command_queue, lock)
 droid.start()
 
 while True:
-    lock.acquire()
-    command_queue.put("S4" + str(random.randint(1100, 1800)))
-    command_queue.put("S4" + str(random.randint(1100, 1800)))
-    lock.release()
-    time.sleep(5)
+    try:
+        command = "S4" + str(random.randint(1100, 1800))
+        lock.acquire()
+        command_queue.put(command)
+        lock.release()
+        if config.DEBUG:
+            print("Main: command added: " + command)
+        time.sleep(5)
+    except KeyboardInterrupt:
+        if config.DEBUG:
+            print("Main: main loop ending")
+            break
 
+if config.DEBUG:
+    print("Main: joining threads")
 command_queue.join()
 droid.stop()
 droid.join()
+
+if config.DEBUG:
+    print("Main: finished")
