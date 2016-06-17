57be264eca7fd27dc9b443a6bc1981ed2a0622b9
Lindsay <lindsayawatt@gmail.com>
2016-06-17 23:07:27 +1000

    typo
    logfile -> log

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def debug(message):
     if config.DEBUG:
         timestamp = dt.datetime.now().strftime("%x-%X: ")
         if (config.DEBUG_MODE == "FILE"):
-            with open('droid_log.txt', 'a') as logfile:
+            with open('droid_log.txt', 'a') as log:
                 log.write(timestamp + message + "\n")
         elif (config.DEBUG_MODE == "PRINT"):
             print(timestamp + message + "\n")
