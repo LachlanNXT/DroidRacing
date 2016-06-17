280dcd702e8cffe3c26bccd5b383355a4dcb845f
Lindsay <lindsayawatt@gmail.com>
2016-06-17 15:02:43 +1000

    Added config.py - contains all adjustable parameters in one place

--- DroidControl.py
+++ DroidControl.py
@@ -1,4 +1,4 @@
-#/
+#!/usr/bin/python3.4
 
 import config
 import serial
---
+++ config.py
@@ -0,0 +1,7 @@
+# global config
+DEBUG = True
+
+# used by DroidControl
+SERIAL_NAME = '/dev/ttyUSB0'
+SERIAL_BAUD_RATE = 9600 # TODO: reflash arduino - increase baud to 115200
+SERIAL_TIMEOUT = 0.1 # seconds
