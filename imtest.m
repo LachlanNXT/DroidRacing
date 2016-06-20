218a177e53a00c44d2fbfa309177e72cfd50ad36
lindsaywatt <lindsay@hyperxnode.com>
2016-06-20 17:02:54 +1000

    matlab script

--- imtest.m
+++ imtest.m
@@ -1,27 +1,30 @@
-test = {{},{},{},{},{},{},{},{},{},{},{}};
+close all;
+startup_rvc;
+
+im = {{},{},{},{},{},{},{},{},{},{},{}};
 blue = {{},{},{},{},{},{},{},{},{},{},{}};
 yellow = {{},{},{},{},{},{},{},{},{},{},{}};
-newim = {{},{},{},{},{},{},{},{},{},{},{}};
-for i = 1:11;
+chroma = {{},{},{},{},{},{},{},{},{},{},{}};
 
-test{i} = iread(strcat('test_image_', num2str(i), '.jpg'), 'double');
-%idisp(test);
-R = test{i}(:,:,1);
-G = test{i}(:,:,2);
-B = test{i}(:,:,3);
-Y = R + G + B;
-r = R./Y; g = G./Y; b = B./Y;
-newim{i} = test{i};
-newim{i}(:,:,1) = r;
-newim{i}(:,:,2) = g;
-newim{i}(:,:,3) = b;
-%figure
-%idisp(newim);
+for i = 1:21;
+    im{i} = iread(strcat('test_image_', num2str(i), '.jpg'), 'double');
+    %idisp(test);
+    R = im{i}(:,:,1);
+    G = im{i}(:,:,2);
+    B = im{i}(:,:,3);
+    Y = R + G + B;
+    r = R./Y;
+    g = G./Y;
+    b = B./Y;
+    
+    chroma{i} = im{i};
+    chroma{i}(:,:,1) = r;
+    chroma{i}(:,:,2) = g;
+    chroma{i}(:,:,3) = b;
 
-blue{i} = (r > 0) & (r < 0.25) & (b > 0.4) & (b < 1);
-%idisp(blue);
-
-yellow{i} = (r > 0.35) & (r < 0.45) & (b > 0) & (b < 0.3);
-%idisp(yellow);
-
+    blue{i} = (r > 0.12) & (r < 0.16) & (g > 0.3) & (g < 0.34) & (b > 0.38) & (b < 0.6);
+    yellow{i} = (r > 0.38) & (r < 0.43) & (g > 0.34) & (g < 0.4) & (b > 0.18) & (b < 0.27);
+    
+    figure(i);  
+    idisp([blue{i} yellow{i}]);
 end
