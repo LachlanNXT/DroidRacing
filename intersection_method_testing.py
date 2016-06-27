abdd77a2054a84ab77c7d6a73d37c3d145919f46
lindsaywatt <lindsay@hyperxnode.com>
2016-06-27 13:13:06 +1000

    more testing with angle method

--- intersection_method_testing.py
+++ intersection_method_testing.py
@@ -16,8 +16,25 @@ def chromaticity(image):
 def colour_threshold(image, low, high):
     return cv2.inRange(image, np.array(low), np.array(high))
 
+def line_coefs(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    return A, B, -C
+
+def intersection(L1, L2):
+    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        x = Dx / float(D)
+        y = Dy / float(D)
+        return x,y
+    else:
+        return False
+
 # load image
-raw_im = cv2.imread("test_image_18.jpg")
+raw_im = cv2.imread("test_image_20.jpg")
 # make image smaller
 h, w = raw_im.shape[:2]
 raw_im = cv2.resize(raw_im, (w/2, h/2), interpolation = cv2.INTER_LINEAR)
@@ -41,8 +58,8 @@ while (1):
     # lines
     lines = cv2.HoughLinesP(colour_mask, config.HOUGH_LIN_RES, config.HOUGH_ROT_RES, config.HOUGH_VOTES, config.HOUGH_MIN_LEN, config.HOUGH_MAX_GAP)
     
-    yellow_angle = 0
-    blue_angle = 0
+    yellow = np.zeros(4, np.int32)
+    blue = np.zeros(4, np.int32)
     yellow_n = 0
     blue_n = 0
     if lines != None:
@@ -51,31 +68,33 @@ while (1):
             angle = np.rad2deg(np.arctan2(y2-y1, x2-x1))
             if config.MIN_LINE_ANGLE < abs(angle) < config.MAX_LINE_ANGLE:
                 if angle > 0:
-                    yellow_angle += abs(angle)
+                    yellow += line[0]
                     yellow_n += 1
                 elif angle < 0:
-                    blue_angle += abs(angle)
+                    blue += line[0]
                     blue_n += 1
                 if config.IMSHOW:
                     cv2.line(im, (x1,y1), (x2,y2), (0,0,255), 1)
 
     if yellow_n > 0:
-        yellow_angle /= yellow_n
+        yellow /= yellow_n
     else:
-        yellow_angle = 25 # replace with last_yellow
+        yellow = np.array([978, 76, 1003, 90]) # replace with last_yellow
 
     if blue_n > 0:
-        blue_angle /= blue_n
+        blue /= blue_n
     else:
-        blue_angle = 25 # replace with last_blue
+        blue = np.array([99, 94, 130, 76]) # replace with last_blue
 
-    centre = abs(yellow_angle - blue_angle)
-    if blue_angle < yellow_angle:
-        centre = -centre
+    yellow_line = line_coefs(yellow[:2], yellow[2:])
+    blue_line = line_coefs(blue[:2], blue[2:])
+    inter = intersection(blue_line, yellow_line)
+    if inter:
+        centre = int(inter[0])
 
-    cv2.line(im, (w/2, h-20), ((w/2), h-120), (255,255,255), 2)
-    cv2.line(im, (w/2, h-20), ((w/2)+int(100*np.cos(np.deg2rad(centre+90))), int((h-20)-100*np.sin(np.deg2rad(centre+90)))), (0,255,0), 2)
-    cv2.circle(im, (int(centre), h - 20), 10, (255,0,0), -1)
+    cv2.line(im, tuple(yellow[:2]), tuple(yellow[2:]), (0, 255, 0), 2)
+    cv2.line(im, tuple(blue[:2]), tuple(blue[2:]), (0, 255, 0), 2)
+    cv2.circle(im, (centre, h - 20), 10, (255,0,0), -1)
     cv2.imshow("im", im)
     cv2.imshow("color_mask without noise", colour_mask)
     if cv2.waitKey(1) & 0xFF == 27:
