89f37cd8105ac9043eb65010ef61680ac2a2c153
lindsaywatt <lindsay@hyperxnode.com>
2016-06-29 10:35:07 +1000

    C chromaticity function

--- DroidVision.py
+++ DroidVision.py
@@ -3,6 +3,7 @@
 import config
 from utils import debug
 import threading
+import ctypes
 import time
 import cv2
 import numpy as np
@@ -24,6 +25,7 @@ class DroidVisionThread(threading.Thread):
         self.mean_angle = 0.0
         self.desired_steering = config.NEUTRAL_STEERING # 0 = left, 0.5 = center, 1 = right
         self.desired_throttle = config.NEUTRAL_THROTTLE # 0 = stop, 0.5 = medium speed, 1 = fastest
+        self.chromaticity = ctypes.cdll.LoadLibrary('/home/pi/DroidRacing/libchromaticity.so').chromaticity
 
     def run(self):
         debug("DroidVisionThread: Thread started")
@@ -41,15 +43,15 @@ class DroidVisionThread(threading.Thread):
             self.grab_frame()
 
             # magic edge detection
-            #median = np.median(self.frame_chroma)
-            #lower = int(max(0, (1.0 - config.SIGMA) * median))
-            #upper = int(min(255, (1.0 + config.SIGMA) * median))
-            #edges = cv2.Canny(self.frame_chroma, lower, upper)
+            median = np.median(self.frame_chroma)
+            lower = int(max(0, (1.0 - config.SIGMA) * median))
+            upper = int(min(255, (1.0 + config.SIGMA) * median))
+            edges = cv2.Canny(self.frame_chroma, lower, upper)
             #cv2.imshow("edges", edges)
 
-            #edges = cv2.dilate(edges, config.BIG_KERNEL, iterations=1)
-            #edges = cv2.erode(edges, config.BIG_KERNEL, iterations=1)
-            #edges = cv2.erode(edges, config.SMALL_KERNEL, iterations=1)
+            edges = cv2.dilate(edges, config.BIG_KERNEL, iterations=1)
+            edges = cv2.erode(edges, config.BIG_KERNEL, iterations=1)
+            edges = cv2.erode(edges, config.SMALL_KERNEL, iterations=1)
 
             #yellow_angle_sum = 0
             #yellow_angle_count = 0
@@ -94,17 +96,9 @@ class DroidVisionThread(threading.Thread):
         self.fps_counter.update()
         # grab latest frame and index the ROI
         self.frame = self.camera.read()[config.ROI_YMIN:config.ROI_YMAX, config.ROI_XMIN:config.ROI_XMAX]
+        self.frame_chroma = self.frame
         # convert to chromaticity colourspace
-        B = self.frame[:, :, 0].astype(np.uint16)
-        G = self.frame[:, :, 1].astype(np.uint16)
-        R = self.frame[:, :, 2].astype(np.uint16)
-        Y = 255 / (B + G + R)
-        #b = (B * Y).astype(np.uint16)
-        #g = (G * Y).astype(np.uint16)
-        #r = (R * Y).astype(np.uint16)
-        #self.frame_chroma = cv2.merge((b,g,r))
-        #self.frame_chroma = np.power(self.frame_chroma, 2)
-        #self.frame_chroma = (self.frame_chroma // 255).astype(np.uint8)
+        chromaticity(ctypes.c_void_p(self.frame), ctypes.c_int(config.NUM_ELEM))
 
     def get_fps(self):
         self.fps_counter.stop()
